2f385a98c93884835ba66c51e94beb453b088007
Gary Fultz <fultz.gary@gmail.com>
2023-05-19 15:17:53 -0400

    merge conflict

--- JPAEventLight/src/main/java/com/skilldistillery/jpaeventlight/entities/User.java
+++ JPAEventLight/src/main/java/com/skilldistillery/jpaeventlight/entities/User.java
@@ -20,9 +20,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-<<<<<<< HEAD
 
-=======
 
 	private String username;
 	private String password;
@@ -204,7 +202,7 @@ public class User {
 				+ ", lastUpdate=" + lastUpdate + ", profilePicture=" + profilePicture + ", aboutMe=" + aboutMe
 				+ ", address=" + address + ", venue=" + venue + ", venueComments=" + venueComments + "]";
 	}
->>>>>>> 28d86247167bdae143a16758c2c67223ab0be9f8
+
 
 
 }
